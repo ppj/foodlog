f080caefc9c4cb835d2f11496b7d190c1d941bbb
ppj <prasanna_3003@yahoo.com>
2014-11-28 23:21:43 +1100

    destroy dependent plates if meal / food is deleted

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,7 +6,7 @@ class Food < ActiveRecord::Base
 
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
 
-  has_many :plates
+  has_many :plates, dependent: :destroy
   has_many :meals, through: :plates
   has_many :comments, as: :commentable, dependent: :destroy
 
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -7,7 +7,7 @@ class Meal < ActiveRecord::Base
   include Voteable
 
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :plates
+  has_many :plates, dependent: :destroy
   has_many :foods, through: :plates
   has_many :comments, as: :commentable, dependent: :destroy
 
